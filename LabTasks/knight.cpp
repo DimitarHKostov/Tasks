ebb5a5b0e3dff6168afeb860569f2987e67258c1
Dimitar Kostov <32267212+DimitarHKostov@users.noreply.github.com>
2020-08-30 18:01:01 +0300

    Add files via upload

---
+++ LabTasks/knight.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <stack>
+
+const int labSize = 8;
+const int dummyValue = 14;
+
+int lab[labSize][labSize] =    {{0,1,0,1,0,0,0,0},
+					{0,1,1,1,0,0,0,0},
+					{0,0,1,0,0,0,0,0},
+					{1,1,1,0,0,0,0,0},
+					{0,1,1,0,0,1,0,0},
+					{0,1,1,0,0,1,0,0},
+					{0,1,1,0,0,1,0,0},
+					{0,1,1,0,0,1,0,0}};
+
+bool validStep(int y, int x){
+	return y>=0 && y<labSize && x>=0 && x<labSize && lab[y][x]==0;
+}
+
+bool knightMovementRecursive(int lab[labSize][labSize], int sy, int sx, int ex, int ey){
+	if(!validStep(sy, sx)){
+		return false;
+	}
+	if(sy==ey && sx==ex){
+		return true;
+	}
+	lab[sy][sx]=dummyValue;
+	if(validStep(sy+2,sx+1)){
+		if(knightMovementRecursive(lab, sy+2, sx+1, ey, ex)){
+			return true;
+		}
+	}
+	if(validStep(sy+2,sx-1)){
+		if(knightMovementRecursive(lab, sy+2,sx-1, ey, ex)){
+			return true;
+		}
+	}
+	if(validStep(sy+1,sx+2)){
+		if(knightMovementRecursive(lab, sy+1,sx+2, ey, ex)){
+			return true;
+		}
+	}
+	if(validStep(sy-1,sx+2)){
+		if(knightMovementRecursive(lab, sy-1,sx+2, ey, ex)){
+			return true;
+		}
+	}
+	if(validStep(sy-2,sx+1)){
+		if(knightMovementRecursive(lab, sy-2,sx+1, ey, ex)){
+			return true;
+		}
+	}
+	if(validStep(sy-2,sx-1)){
+		if(knightMovementRecursive(lab, sy-2,sx-1, ey, ex)){
+			return true;
+		}
+	}
+	if(validStep(sy+1,sx-2)){
+		if(knightMovementRecursive(lab, sy+1,sx-2, ey, ex)){
+			return true;
+		}
+	}
+	if(validStep(sy-1,sx-2)){
+		if(knightMovementRecursive(lab, sy-1,sx-2, ey, ex)){
+			return true;
+		}
+	}
+	return false;
+}
+//------------------------------------------
+// same but using stack instead of recursion
+//------------------------------------------
+
+struct Position{
+	int x;
+	int y;
+};
+
+bool knightMovementIterative(int lab[labSize][labSize], int sy, int sx, int ex, int ey){
+	std::stack<Position> s;
+	s.push({sy, sx});
+	while(!s.empty()){
+		Position out = s.top(); s.pop();
+		if(out.y==ey && out.x==ex){
+			return true;
+		}
+		if(validStep(out.y, out.x)){
+			lab[out.y][out.x]=dummyValue;
+			s.push({out.y+2, out.x+1});
+			s.push({out.y+2, out.x-1});
+			s.push({out.y+1, out.x+2});
+			s.push({out.y-1, out.x+2});
+			s.push({out.y-2, out.x+1});
+			s.push({out.y-2, out.x-1});
+			s.push({out.y+1, out.x-2});
+			s.push({out.y-1, out.x-2});
+		}
+	}
+	return s.size() > 0;
+}
+
+int main(){
+	//std::cout << knightMovementRecursive(lab, 0, 0, 7, 7) << std::endl;
+	std::cout << knightMovementIterative(lab, 0, 0, 7, 7) << std::endl;
+	return 0;
+}
